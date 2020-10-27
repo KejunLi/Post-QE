54f0d7c2c30669a7438a8b1f271c8bec82f1e49b
KejunLi <likejun622@gmail.com>
2020-10-27 00:22:40 -0700

    move coordinate_conversion.py to write_crystaxes_pos.py

--- write/write_crystaxes_pos.py
+++ write/write_crystaxes_pos.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 import os
 
-def cnv_cartcoord_cryst(cryst_axes, atoms, ap_cart_coord):
+def write_crystaxes_pos(cryst_axes, atoms, atomic_pos):
     """
     =---------------------------------------------------------------------------
     +   convert atomic positions in cartesian coordinates to crystal fractional
@@ -14,12 +14,14 @@ def cnv_cartcoord_cryst(cryst_axes, atoms, ap_cart_coord):
     +   ap_cart_coord: atomic positions in cartesian coordinates, angstrom
     =---------------------------------------------------------------------------
     """
-    inv_cryst_axes = np.linalg.inv(cryst_axes)
-    atomic_pos = np.matmul(ap_cart_coord, inv_cryst_axes)
     atoms_atomic_pos = np.column_stack((atoms, atomic_pos))
 
-    output_file = open("/home/likejun/work/write_files/cnv/cnv.txt", "w")
-    output_file = open("/home/likejun/work/write_files/cnv/cnv.txt", "a")
+    output_file = open(
+        "/home/likejun/work/write_files/convert_cart_to_cryst/cnv.txt", "w"
+    )
+    output_file = open(
+        "/home/likejun/work/write_files/convert_cart_to_cryst/cnv.txt", "a"
+    )
     output_file.write("convert cart_coord to cryst_coord\n")
     output_file.write("CELL_PARAMETERS angstrom\n")
     np.savetxt(output_file, cryst_axes, "%.10f")
