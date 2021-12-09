36db0c083166603951112743c02d0c9d81f288bf
kejunli <likejun622@gmail.com>
2021-12-09 12:12:12 -0800

    enable to save as xyz

--- rotate_cell.py
+++ rotate_cell.py
@@ -47,7 +47,9 @@ def rotation_mat(alpha, beta, gamma):
     rot = np.matmul(rotation_z, rot)
     return rot
 
-def write_xsf(cell_parameters, atoms, atomic_pos_cryst, outfile="default.xsf"):
+def write_xsf(
+    cell_parameters, atoms, atomic_pos_cryst, outfile="after_rotation.xsf"
+):
     """
     =---------------------------------------------------------------------------
     +   This function writes the atoms and atomic positions for visualizing
@@ -63,7 +65,6 @@ def write_xsf(cell_parameters, atoms, atomic_pos_cryst, outfile="default.xsf"):
     atomic_pos_cart = np.matmul(atomic_pos_cryst, cell_parameters)
     atoms_atomic_pos_cart = np.column_stack((atoms, atomic_pos_cart))
     nat = len(atoms)
-    # path = os.path.join("/home/lkj/work/write_files/molecule", outfile)
     path = os.path.join(os.getcwd(), outfile)
     outfile = open(path, "w")
     outfile = open(path, "a")
@@ -75,25 +76,72 @@ def write_xsf(cell_parameters, atoms, atomic_pos_cryst, outfile="default.xsf"):
     np.savetxt(outfile, atoms_atomic_pos_cart, "%s")
     outfile.close()
 
+
+def write_xyz(atoms, atomic_pos_cart, outfile="after_rotation.xyz"):
+    """
+    =---------------------------------------------------------------------------
+    +   This function writes the atoms and atomic positions for visualizing
+    +   the fixing range by VESTA
+    +
+    +   type(input): <class 'numpy.ndarray'>
+    +   atoms (atomic species associated with each atomic position)
+    +   atomic_pos_cart (atomic positions)
+    =---------------------------------------------------------------------------
+    """
+    atoms_atomic_pos_cart = np.column_stack((xyz.atoms, atomic_pos_cart))
+    nat = len(atoms)
+    path = os.path.join(os.getcwd(), outfile)
+    print("Write to file:", path)
+    outfile = open(path, "w")
+    outfile = open(path, "a")
+    outfile.write(str(nat) + "\n\n")
+    np.savetxt(outfile, atoms_atomic_pos_cart, "%s")
+    outfile.close()
+
+
 if __name__ == "__main__":
-    # path = "/home/lkj/work/2d_perovskites/racemic-NPB/opium/8_scf_wsoc_pbe/scf.in"
+    parser = argparse.ArgumentParser(
+    description="Rotate cell"
+    )
+    parser.add_argument(
+        "alpha", type=float, nargs="?", default=0.0, help='rotation w.r.t. x'
+    )
+    parser.add_argument(
+        "beta", type=float, nargs="?", default=0.0, help='rotation w.r.t. y'
+    )
+    parser.add_argument(
+        "gamma", type=float, nargs="?", default=0.0, help='rotation w.r.t. z'
+    )
+    args = parser.parse_args()
+    alpha_x = args.alpha
+    beta_y = args.beta
+    gamma_z = args.gamma
+
     cwd = os.getcwd()
     for f in os.listdir(cwd):
         if f == "scf.in" or f == "relax.in":
             qe = qe_in(os.path.join(cwd, f))
+            rotation_mat = rotation_mat(
+                alpha=alpha_x, beta=beta_y, gamma=gamma_z
+            )
+            inv_rotation_mat = np.linalg.inv(rotation_mat)
+            # to rotate a supercell, one only needs to rotate the cell parameters
+            cell_parameters = np.matmul(qe.cell_parameters, inv_rotation_mat)
 
-    rotation_mat = rotation_mat(alpha=90, beta=180, gamma=0)
+            write_xsf(
+                cell_parameters=cell_parameters, 
+                atoms=qe.atoms, 
+                atomic_pos_cryst=qe.atomic_pos_cryst,
+                outfile="after_rotation.xsf"
+            )
+        elif f.endswith(".xyz") and not f.startswith("after"):
+            xyz = read_xsf_xyz(os.path.join(cwd, f))
+            print("Read file: ", os.path.join(cwd, f))
+            rotation_mat = rotation_mat(
+                alpha=alpha_x, beta=beta_y, gamma=gamma_z
+            )
+            inv_rotation_mat = np.linalg.inv(rotation_mat)
+            # rotate the atomic positions of a molecule in cartesian coordinate
+            atomic_pos_cart = np.matmul(xyz.atomic_pos_cart, inv_rotation_mat)
 
-    inv_rotation_mat = np.linalg.inv(rotation_mat)
-
-    # to rotate a supercell, one only needs to rotate the cell parameters
-    cell_parameters = np.matmul(qe.cell_parameters, inv_rotation_mat)
-
-    write_xsf(
-        cell_parameters=cell_parameters, 
-        atoms=qe.atoms, 
-        atomic_pos_cryst=qe.atomic_pos_cryst,
-        outfile="rot.xsf"
-    )
-
-
+            write_xyz(atoms=xyz.atoms, atomic_pos_cart=atomic_pos_cart)
